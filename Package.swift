92a6915f2399a7ccdd1ca716aea81ea54cc7af1e
Stephen <stephen@sstctf.org>
2016-11-04 16:14:03 -0700

    Renamed package

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription;
 
 let package = Package(
-  name: "publisher-daemon",
+  name: "publishd",
   dependencies: [
     .Package(url: "https://github.com/czechboy0/Socks.git", majorVersion: 0, minor: 12),
     .Package(url: "https://github.com/kareman/SwiftShell", "3.0.0-beta"),
